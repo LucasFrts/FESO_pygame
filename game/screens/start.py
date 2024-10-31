f7ea2e33d86611ad614cab37ced9eb2962461ff5
LucasFrts <lmrfreitas9@gmail.com>
2024-10-31 20:26:30 -0300

    .

--- game/screens/start.py
+++ game/screens/start.py
@@ -3,5 +3,5 @@ class StartMenu(Container):
     def setup(self):
         self.setting.screen.fill("#0b0b0b")
         text_surface = self.setting.fonts.medium.render("BreakCode", True, "white")
-        self.setting.screen.blit(text_surface, (40, 250))
+        self.setting.screen.blit(text_surface, (self.setting.screen.get_width() / 2, self.setting.screen.get_height() / 2))
 
